d4d563d2214af21853d01018f0f2a3b6defdf4ab
Apollo Tang <tang.apollo@gmail.com>
2017-03-25 00:48:28 -0400

    added chapter 6 to 11

---
+++ b-06-00-todo-item--toggle/list.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import _ from 'lodash';
+
+
+const TodoItem = (props) => {
+    const style_row = {
+        display:'inline-block',
+        width: '100%'
+    }
+    const style_text = {
+        display:'inline-block',
+        width: '170px'
+    }
+    return(
+        <div style={style_row} >
+            <span
+                style={style_text}
+                onClick={()=>props.toggleTodo(props.todoItem.id)} > {/* <---[!] */}
+                {props.todoItem.text}
+            </span>
+        </div>
+    );
+};
+TodoItem.propTypes = {
+    todoItem: React.PropTypes.object,
+    toggleTodo: React.PropTypes.func // <--- [!]
+}
+
+
+const TodoList = (props) => {
+    const todoList = _.isArray(props.todoList) ? props.todoList: [];
+
+    return (
+        <div>
+            {
+                todoList.map( (item, index) => {
+                    return(
+                        <TodoItem
+                            key={index}
+                            todoItem={item}
+                            toggleTodo={(id)=>{console.log('toggle todo :', id);}}  /* <---[!] */
+                            />
+                    )
+                })
+            }
+        </div>
+    );
+}
+TodoList.propTypes = {
+    todoList: React.PropTypes.array
+}
+
+
+export default TodoList;
+
+
