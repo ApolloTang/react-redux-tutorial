242a142d531be544c077b434d00f62e2e3c04f95
Apollo Tang <apollotang@lights.local>
2017-04-05 23:27:07 -0400

    part-2, added initial (previous) app folder

---
+++ part-2-todo-list-app/00-00-previous/app.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import List from './list';
+
+class App extends React.Component {
+    constructor(props) {
+        super();
+        this.handle_addItem = this.handle_addItem.bind(this);
+        this.state = {
+            list: []
+        };
+    }
+
+    handle_addItem() {
+        const list_prev = this.state.list;
+        const list_next = [].concat(list_prev);
+
+        const item_new = Date.now()+'';
+
+        list_next.push(item_new);
+
+        this.setState({
+            list: list_next
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handle_addItem} >Add Item</button>
+                <List list={this.state.list} />
+            </div>
+        );
+    }
+};
+
+
+export default App;
+
+
---
+++ part-2-todo-list-app/00-00-previous/list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Item = (props) => <div>{props.item}</div>;
+Item.propTypes = {
+    item: React.PropTypes.string
+}
+
+
+const List = (props) => {
+    const list
+        = (Object.prototype.toString.call(props.list) === '[object Array]' )
+        ? props.list : [];
+
+    return (
+        <div>
+            { list.map( item => <Item key={item} item={item} />) }
+        </div>
+    );
+}
+List.propTypes = {
+    list: React.PropTypes.array
+}
+
+
+export default List;
+
+
